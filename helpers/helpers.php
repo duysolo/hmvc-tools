e49205df5e2a7e6312c2c14b71e521f3409bb744
Tedozi Manson <duypt.dev@gmail.com>
2020-04-01 00:16:56 +0800

    remove unused functions

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,20 +1,5 @@
 <?php
 
-if (!function_exists('split_files_with_basename')) {
-    /**
-     * @param array $files
-     */
-    function split_files_with_basename(array $files, $suffix = '.php')
-    {
-        $result = [];
-        foreach ($files as $row) {
-            $baseName = basename($row, $suffix);
-            $result[$baseName] = $row;
-        }
-        return $result;
-    }
-}
-
 if (!function_exists('json_encode_prettify')) {
     /**
      * @param array $files
